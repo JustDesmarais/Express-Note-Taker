95bcfd19e3eebe24772b041ba6364fce27970e13
JustDesmarais <143149797+JustDesmarais@users.noreply.github.com>
2023-11-20 09:48:13 -0600

    update gitignore

--- server.js
+++ server.js
@@ -5,3 +5,16 @@ const app = express();
 const PORT = 3001
 
 app.use(express.static('public'));
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
