51ce0c91e9a20745f86de3267bad32aaaa8ac827
JustDesmarais <143149797+JustDesmarais@users.noreply.github.com>
2023-11-21 10:31:29 -0600

    POST method appending notes db

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const fs = require('fs')
+const fs = require('fs');
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
@@ -16,7 +17,7 @@ app.use(express.static('public'));
 //);
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
@@ -43,6 +44,7 @@ app.post('/api/notes', (req, res) => {
         body: newNote,
       };
   
+      readAndAppend(newNote, './db/db.json')
       console.log(response);
       res.status(201).json(response);
     } else {
